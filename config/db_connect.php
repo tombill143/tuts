cb584abf4db5b11af5c8e694de17d71d88c9330f
tom <tom@twentyfour.dk>
2024-11-14 15:31:03 +0100

    Update database connection to use JawsDB on Heroku

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,4 +1,4 @@
-<?php 
+<!-- <?php 
 
     //connect to the database 
     $conn = mysqli_connect('localhost', 'tom', 'test1234', 'pizza_byte');
@@ -9,5 +9,26 @@
         echo "An Annoying Connection error occurred when connecting to the pizza database: " . mysqli_connect_error();
     }
 
+
+?> -->
+
+<?php 
+
+// Get the database connection URL from Heroku's JawsDB add-on
+$jawsdb_url = parse_url(getenv('JAWSDB_URL'));
+
+// Extract the necessary components of the connection string
+$db_host = $jawsdb_url['host'];
+$db_user = $jawsdb_url['user'];
+$db_password = $jawsdb_url['pass'];
+$db_name = ltrim($jawsdb_url['path'], '/');
+
+// Connect to the database
+$conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+// Check if the connection was successful
+if(!$conn) {
+    echo "An annoying connection error occurred when connecting to the pizza database: " . mysqli_connect_error();
+}
 
 ?>
